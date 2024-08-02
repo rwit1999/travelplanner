e167e7a4ee728a46562579982b89baa513303016
rwit1999 <wribobeats@gmail.com>
2024-08-02 17:31:43 +0530

    Fix issues with login API route

--- src/app/api/admin/create-job/route.ts
+++ src/app/api/admin/create-job/route.ts
@@ -1,6 +1,6 @@
-import { prisma } from '@/lib';
+import prisma from '../../../../lib/prisma'
 import { NextResponse } from 'next/server'
-import { jobsQueue } from '@/lib/queue'
+import { jobsQueue } from '../../../../lib/queue'
 
 export async function POST(req:Request) {
     try{
--- src/app/api/admin/login/route.ts
+++ src/app/api/admin/login/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { SHA256 as sha256 } from "crypto-js";
 import { SignJWT } from "jose";
-import { prisma } from '@/lib';
+import prisma from '../../../../lib/prisma'
 
 import { cookies } from "next/headers";
 
